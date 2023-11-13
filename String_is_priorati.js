84769508ceea8c5e63944826118db703d1d7d318
arman52114 <129601164+arman52114@users.noreply.github.com>
2023-11-13 22:34:44 +0600

    first commit

---
+++ String_is_priorati.js
@@ -0,0 +1,5 @@
+  let n=5;
+  let c="Char"
+  let CharN=n+c;
+
+  console.log(CharN);
---
+++ object.js
@@ -0,0 +1,11 @@
+  const myObject={
+    name: "arman",
+    roll: 3,
+    work: function(){
+        console.log("unemployment");
+        return 1;
+        // in javaScript although we return nothing but function will return undefined
+    },
+  }
+
+  console.log(myObject.roll)
